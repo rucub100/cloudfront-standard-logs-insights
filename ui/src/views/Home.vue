b427d8366030e92a10638ae152a810cf11164f39
Ruslan Curbanov <rucub100@hhu.de>
2021-12-12 14:34:44 +0100

    ui: prepare nav tabs 'Overview' & 'Table'

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="container home">
-        <img alt="Vue logo" src="../assets/logo.png" />
+        <h1 class="mt-4 mb-3">CloudFront Standard Logs Insights</h1>
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"
+                    >Overview</a
+                >
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Table</a>
+            </li>
+        </ul>
     </div>
 </template>
 
